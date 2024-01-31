1478b2faeb8b24d09a41350ea08f41b5a6f93892
ｍｙｙ <mayu.takabayashi@basesystem.co.jp>
2024-01-31 10:52:04 +0900

    💡 ブロックの代わりに画像を消すように

--- breakout.js
+++ breakout.js
@@ -1,32 +1,98 @@
-var canvas = document.getElementById("breakoutCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
-var rightPressed = false;
-var leftPressed = false;
-var brickRowCount = 5;
-var brickColumnCount = 3;
-var brickWidth = 75;
-var brickHeight = 20;
-var brickPadding = 10;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 30;
-var score = 0;
-var lives = 3;
+// ボールとか用のキャンバス
+let canvas = document.getElementById("breakoutCanvas");
+let ctx = canvas.getContext("2d");
 
-var bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
+const baseImgPath = "img/base.png";
+const brickImgPath = "img/cover.png";
+
+const ballRadius = 10;
+const paddleHeight = 10;
+const paddleWidth = 75;
+const brickWidth = 20;
+const brickHeight = 20;
+const brickPadding = 0;
+const brickOffsetTop = 30;
+const brickOffsetLeft = 30;
+
+let x = canvas.width/2;
+let y = canvas.height-30;
+let dx = 2;
+let dy = -2;
+
+let paddleX = (canvas.width-paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
+
+let score = 0;
+let lives = 10;
+
+const bricksMask = 
+[
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[1,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1],
+[0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,0,0,0,0,1,1,1,1]];
+
+function initCanvas(image,imagePath, c){
+
+    image.addEventListener("load",function (){
+
+        c.drawImage(image, brickOffsetLeft, brickOffsetTop); 
+
+    });
+    image.src = imagePath;
+}
+
+
+// 背景用画像を書く
+const baseCanvas = document.getElementById("baseCanvas");      
+let baseCtx = baseCanvas.getContext("2d");
+
+const baseImage = new Image();
+initCanvas(baseImage,baseImgPath, baseCtx);
+
+// ブロック用画像を書く
+const brickCanvas = document.getElementById("bricksCanvas");      
+let brickCtx = brickCanvas.getContext("2d");
+
+const brickImage = new Image();
+initCanvas(brickImage, brickImgPath, brickCtx);
+
+// 仮想的にブロックを作る
+let bricks = [];
+
+const brickRowCount = brickCanvas.width/brickWidth;
+const brickColumnCount = brickCanvas.height/brickHeight;
+console.log('行：'+ brickRowCount);
+console.log('列' + brickColumnCount);
+
+for(let c=0; c<brickColumnCount; c++) {
     bricks[c] = [];
-    for(var r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    for(let r=0; r<brickRowCount; r++) {
+        // マスクの数が足りない可能性も一応考えておく
+        let s = bricksMask.length <= c || bricksMask[0].length <= r ? 0: bricksMask[c][r];
+
+        bricks[c][r] = { x: 0, y: 0, status: s };
     }
 }
+
+
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -49,15 +115,15 @@ function keyUpHandler(e) {
     }
 }
 function mouseMoveHandler(e) {
-    var relativeX = e.clientX - canvas.offsetLeft;
+    let relativeX = e.clientX - canvas.offsetLeft;
     if(relativeX > 0 && relativeX < canvas.width) {
         paddleX = relativeX - paddleWidth/2;
     }
 }
 function collisionDetection() {
-    for(var c=0; c<brickColumnCount; c++) {
-        for(var r=0; r<brickRowCount; r++) {
-            var b = bricks[c][r];
+    for(let c=0; c<brickColumnCount; c++) {
+        for(let r=0; r<brickRowCount; r++) {
+            let b = bricks[c][r];
             if(b.status == 1) {
                 if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
                     dy = -dy;
@@ -88,21 +154,45 @@ function drawPaddle() {
     ctx.closePath();
 }
 function drawBricks() {
-    for(var c=0; c<brickColumnCount; c++) {
-        for(var r=0; r<brickRowCount; r++) {
-            if(bricks[c][r].status == 1) {
-                var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
-                var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+
+    for(let c=0; c<brickColumnCount; c++) {
+        for(let r=0; r<brickRowCount; r++) {
+            
+            if(bricks[c][r].status == 1 &&bricksMask[c][r] ) {
+
+                let brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
                 bricks[c][r].x = brickX;
                 bricks[c][r].y = brickY;
-                ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
-                ctx.fill();
-                ctx.closePath();
+                //ctx.beginPath();
+                //ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                //ctx.fillStyle = "rgba(0, 255, 0, 0.5)";
+                //ctx.fill();
+                //ctx.closePath();
+
+                
+            }else{
+                // 当たったら画像を透過する
+                let brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+                let brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                let imgData2 = brickCtx.getImageData(brickX, brickY, brickWidth, brickHeight);
+
+                for(let i = 0, len = brickWidth * brickHeight; i < len; i++){
+                    //ピクセル
+                    const p = i * 4;
+                    //alpha値を0に
+                    imgData2.data[p + 3] = 0;
+                }
+
+                brickCtx.putImageData(imgData2, brickX, brickY);
+                
+            
             }
         }
     }
+
 }
 function drawScore() {
     ctx.font = "16px Arial";
